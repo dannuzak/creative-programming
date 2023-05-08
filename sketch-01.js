bcf4cc5d28e596db744dc688156b9fb09795a254
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-05-08 21:38:56 +0100

    changed values

--- sketch-01.js
+++ sketch-01.js
@@ -1,33 +1,43 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 600, 600 ]
+/*   dimensions: 'A4',   // [ 600, 600 ],
+  pixelsPerInch: 300,
+  orientation: 'landscape'	 */
+  dimensions: [ 1000, 1000 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {  // drawing a big white rectangle
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);	
-
+/* 
 	const w = 60;
 	const h = 60; 
-	const gap = 20;
+	const gap = 20; */
+	const w = width * 0.10;
+	const h = height * 0.10;
+	const gap = width * 0.03;
+	const ix = width * 0.17;
+	const iy = height * 0.17;
+	context.lineWidth = width * 0.01;
+	
+	const off = width * 0.02; 
 	let x, y;
 
 	for (let i = 0; i < 5; i++){
         for(let j = 0; j < 5; j++) {
-			let x = 100 + (w + gap) * i;
-			let y = 100 + (h + gap) * j;
+			let x = ix + (w + gap) * i;
+			let y = iy + (h + gap) * j;
 
 			context.beginPath(); //the number after the + is where the next square will start
 			context.rect(x,y,w,h);
-			//context.rect(100 + 110 * i,100,50,50);
 			context.stroke();
 
-			if (Math.random() > 0.5) {
+			if (Math.random() > 0.5) { //adding random inner squares
 				context.beginPath();
-				context.lineWidth = 2;
-				context.rect(x + 8, y + 8, w - 16, h - 16);
+				context.rect(x + off / 2, y + off / 2, w - off, h - off);
+			
 				context.stroke();
 			}
 		}
