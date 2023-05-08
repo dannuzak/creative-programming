5fe72cda80979eaee978974a235e72f0f4b9bd8e
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-05-08 19:45:43 +0100

    drawing a grid of squares

--- sketch-01.js
+++ sketch-01.js
@@ -1,13 +1,31 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 600, 600 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {  // drawing a big white rectangle
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);	
+
+	const w = 60;
+	const h = 60; 
+	const gap = 20;
+	let x, y;
+
+	for (let i = 0; i < 5; i++){
+        for(let j = 0; j < 5; j++) {
+			let x = 100 + (w + gap) * i;
+			let y = 100 + (h + gap) * j;
+
+			context.beginPath(); //the number after the + is where the next square will start
+			context.rect(x,y,w,h);
+			//context.rect(100 + 110 * i,100,50,50);
+			context.stroke();
+		}
+	}
+
   };
 };
 
