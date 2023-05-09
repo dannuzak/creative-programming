267ee6063d21cd1c55dd2013098c8df3c65039c5
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-05-09 19:28:10 +0100

    watch

--- sketch-02.js
+++ sketch-02.js
@@ -1,35 +1,46 @@
 const canvasSketch = require('canvas-sketch');
 
-const settings = {
-  dimensions: [ 1000, 1000 ]
-};
-
-const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
-
-    context.fillStyle = "black";
-
-    const x = width * 0.5;
-    const y = height * 0.5;
-    const w = width * 0.3;
-    const h = height * 0.3;
-
-    context.save(); //we save the state first
-    context.translate(x, y);
-    context.rotate(0.3);
-
-    context.beginPath();
-    context.rect(w * 0.5, h * 0.5, w, h);
-    context.fill();
-    context.restore(); // to avoid the translate to accumulate
-
-    context.translate(0, 0);
-
-    context.beginPath();
-    context.arc(0, 0, 50, 0, Math.PI * 2);
-    context.fill();
+    const settings = {
+      dimensions: [ 1080, 1080 ]
+    };
+    
+    const degToRad = degrees => {
+      return degrees / 180 * Math.PI;
+    }
+    
+    const sketch = () => {
+      return ({ context, width, height }) => {
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, width, height);
+    
+        context.fillStyle = 'black';
+    
+        const cx = width * 0.5;
+        const cy = height * 0.5;
+    
+        const w = width * 0.01;
+        const h = height * 0.1;
+        let x, y;
+        
+        const num = 12;
+        const radius = width * 0.3;
+    
+        for (let i = 0; i < num; i++) { // we need to change the angle of rotation based on the index of the loop
+          const slice = degToRad(360 / num);
+          const angle = slice * i;
+    
+          x = cx + radius * Math.sin(angle);
+          y = cy + radius * Math.cos(angle);
+    
+          context.save();
+          context.translate(x,y);
+          context.rotate(-angle);
+      
+          context.beginPath();
+          context.rect(-w * 0.5, -h * 0.5, w, h);
+          context.fill();
+          context.restore();
+        } 
   };
 };
 
