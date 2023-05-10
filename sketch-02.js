f113c9c5517811b809f2e7fb30f3c65ae8c5c751
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-05-10 16:21:46 +0100

    finished sketch unit 3

--- sketch-02.js
+++ sketch-02.js
@@ -1,4 +1,6 @@
-const canvasSketch = require('canvas-sketch');
+/* FINAL EXERCISE */
+
+/* const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
 
@@ -53,6 +55,73 @@ const random = require('canvas-sketch-util/random');
           context.rotate(-angle);
 
           context.lineWidth = random.range(5, 20);
+          context.beginPath();
+          context.arc(0,0, radius * random.range(0.7, 1.3), slice * random.range(1, -8), slice * random.range(1, 5));
+          context.stroke();
+
+          context.restore();
+        } 
+  };
+};
+
+canvasSketch(sketch, settings); */
+
+
+const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+
+    const settings = {
+      dimensions: [ 1080, 1080 ]
+    };
+    
+    const randomRange = (min, max) => {
+      return Math.random() * (max - min) + min;
+    }
+
+    const degToRad = degrees => {
+      return degrees / 180 * Math.PI;
+    }
+    
+    const sketch = () => {
+      return ({ context, width, height }) => {
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, width, height);
+    
+        context.fillStyle = 'black';
+    
+        const cx = width * 0.5;
+        const cy = height * 0.5;
+    
+        const w = width * 0.01;
+        const h = height * 0.1;
+        let x, y;
+        
+        const num = 40;
+        const radius = width * 0.3;
+    
+        for (let i = 0; i < num; i++) { // we need to change the angle of rotation based on the index of the loop
+          const slice = math.degToRad(360 / num);
+          const angle = slice * i;
+    
+          x = cx + radius * Math.sin(angle);
+          y = cy + radius * Math.cos(angle);
+    
+          context.save();
+          context.translate(x,y);
+          context.rotate(-angle);
+          context.scale(random.range(0.1, 2), random.range(0.2, 0.5)) // for numbers between 1 and 3
+      
+          context.beginPath();
+          context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
+          context.fill();
+          context.restore();
+
+          context.save();
+          context.translate(cx,cy);
+          context.rotate(-angle);
+
+          context.lineWidth = random.range(3, 10);
           context.beginPath();
           context.arc(0,0, radius * random.range(0.7, 1.3), slice * random.range(1, -8), slice * random.range(1, 5));
           context.stroke();
