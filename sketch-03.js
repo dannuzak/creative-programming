b0b33130b4f99bcd4e2c99f78955b3c5521951fc
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-05-10 17:24:45 +0100

    adding pointA

--- sketch-03.js
+++ sketch-03.js
@@ -1,14 +1,35 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    const point = { x: 800, y: 400, radius: 10 };
+    
+
+    context.beginPath();
+    context.arc(point.x, point.y, point.radius, 0, Math.PI * 2)
+    context.fillStyle = 'black';
+    context.fill();
+
   };
 };
 
 canvasSketch(sketch, settings);
+
+//Classes are recipes for writting objects
+
+class Point {
+  constructor(x,y,radius){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+
+  }
+
+}
