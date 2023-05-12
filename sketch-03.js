e454d47a904d9c01816c4b45e0c4704592943ee1
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-05-12 15:33:39 +0100

    created one point

--- sketch-03.js
+++ sketch-03.js
@@ -1,69 +1,36 @@
 const canvasSketch = require('canvas-sketch');
-const random = require('canvas-sketch-util/random')
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
 };
 
-const sketch = ({context, width, height}) => {
-  const agents = [];
-
-  for(let i = 0; i < 40; i++){
-    const x = random.range(0, width);
-    const y = random.range(0, width);
-
-    agents.push(new Agent(x,y));
-  }
-
+const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    agents.forEach(agent => {
-      agent.draw(context);
-    });
 
-   /*  const pointA = new Point(800, 400, 10 );
-    const pointB = new Point(300, 700, 10 );
+    const point = { x: 800, y: 400, radius: 10 };
 
-    agentA.draw(context);
-    agentB.draw(context); */
-    
-    /* context.beginPath();
-    context.arc(pointA.x, pointA.y, pointA.radius, 0, Math.PI * 2);
-    context.fillStyle = 'black';
-    context.fill();
-
-    context.beginPath();
-    context.arc(pointB.x, pointB.y, pointB.radius, 0, Math.PI * 2);
-    context.fill(); */
+  context.beginPath();
+  context.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
+  context.fillStyle = 'black';
+  context.fill();
 
   };
+
+  
 };
 
 canvasSketch(sketch, settings);
 
-//Classes are recipes for writting objects
-
+//whenever we need multiple instances of the same classes we can use classes
+//constructor method and we can pass params
+//with this we are referring to the scope of this class
 class Point {
-  constructor(x,y,radius){
+  constructor(x,y,radius) {
     this.x = x;
     this.y = y;
     this.radius = radius;
   }
 }
-
-class Agent { // use agent for anything that we want to draw, only y x, radius is not needed
-  constructor() {
-    this.pos = new Point();
-    this.radius = 10;
-  }
-
-  draw(context) {
-    context.fillStyle = 'black';
-    context.beginPath();
-    context.arc(this.pos.x, this.radius, 0, Math.PI * 2);
-    context.fill();
-  }
-}
-
