ae8ec2fb20b254ac6087a21c872afab9bd968947
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-05-17 15:10:19 +0100

    mapped scale with mapRange

--- sketch-noise-05.js
+++ sketch-noise-05.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math')
 
 const settings = {
   dimensions: [ 1080, 1080 ]
@@ -26,32 +27,37 @@ const sketch = () => {
 // we are not going to use 2 for loops this time but maths
 
 for (let i = 0; i < numCells; i++) {
- const col = i % cols;
- const row = Math.floor(i / cols);
+  const col = i % cols;
+  const row = Math.floor(i / cols);
 
- const x = col * cellw;
- const y = row * cellh;
- const w = cellw * 0.8;
- const h = cellh * 0.8;
+  const x = col * cellw;
+  const y = row * cellh;
+  const w = cellw * 0.8;
+  const h = cellh * 0.8;
 
+  const n = random.noise2D(x,y, 0.001); // noise2d gives us anumber between -1 and 1 and when multiplied by Math.PI we get the equivalent of - 180 degrees to 180 degrees and to see it we need to rotate our context by angle
+  //Right now the frequency is too high, because of the big dif between x and y, we fix adding the 3rd param in noise which is frequency. We also add amplitud, not as 4th param of noise, but to angle and the lines start flowing nicely.
 
+  const angle = n * Math.PI * 0.2;  
 
-  const n = random.noise2D(x,y); // noise2d gives us anumber between -1 and 1 and when multiplied by Math.PI we get the equivalent of - 180 degrees to 180 degrees and to see it we need to rotate our context by angle
+  //const scale = (n + 1) / 2 * 30;
+  //const scale = (n * 0.5 + 0.5) * 30;
+  const scale = math.mapRange(n, -1, 1, 1, 30);
 
-  const angle = n * Math.PI;  
+  context.save();
+  context.translate(x,y);
+  context.translate(margx, margy);
+  context.translate(cellw * 0.5, cellh * 0.5);
+  context.rotate(angle);
 
- context.save();
- context.translate(x,y);
- context.translate(margx, margy);
- context.translate(cellw * 0.5, cellh * 0.5);
- context.rotate(angle);
+  context.lineWidth = scale;
 
- context.beginPath();
- context.moveTo(w * -0.5, 0);
- context.lineTo(w * 0.5, 0);
- context.stroke();
+  context.beginPath();
+  context.moveTo(w * -0.5, 0);
+  context.lineTo(w * 0.5, 0);
+  context.stroke();
 
- context.restore();
+  context.restore();
 }
 
   };
