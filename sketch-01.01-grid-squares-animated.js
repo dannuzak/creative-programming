ac8f65a7706f722e6154cc4310d4d1afbfb6bbf9
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-06-08 19:16:27 +0100

    animated grid of squares, but needs to be improved, speed gradually slows down

--- sketch-01.01-grid-squares-animated.js
+++ sketch-01.01-grid-squares-animated.js
@@ -1,8 +1,10 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1000, 1000 ],
-};
+	dimensions: [1000, 1000],
+	animate: true,
+	duration: 5,
+  };
 
 class Grid {
 	constructor(w, h, gap, ix, iy, off) {
@@ -26,59 +28,69 @@ class Square {
 		this.x = x;
 		this.y = y;
 	}
- 
-	draw(context){
+	draw(context) {
 		context.save();
-		context.beginPath(); //the number after the + is where the next square will start
+		context.beginPath();
 		context.strokeStyle = lightBlue;
 		context.rect(this.x, this.y, this.grid.w, this.grid.h);
 		context.fillStyle = darkBlue;
 		context.fill();
 		context.stroke();
-
-		if (Math.random() > 0.5) { 
-			context.beginPath();
-			context.fillStyle = orange;
-			context.rect(this.x + this.grid.off / 2, this.y + this.grid.off / 2, this.grid.w - this.grid.off, this.grid.h - this.grid.off);
-			context.stroke();
-			context.fill();	
-		}
-	}
-}
-
-const sketch = () => {
-  return ({ context, width, height }) => { 
-	 
-    context.fillStyle = brown; 
-    context.fillRect(0, 0, width, height);
-
-	const w = width * 0.10;  
-	const h = height * 0.10;  
-	const gap = width * 0.03; 
-	const ix = width * 0.17; 
-	const iy = height * 0.17;
-
-	const off = width * 0.02; 
 	
-	context.lineWidth = width * 0.01;
+		if (Math.random() > 0.5) {
+		  context.beginPath();
+		  context.fillStyle = orange;
+		  const offset = this.grid.off / 2;
+		  context.rect(
+			this.x + offset,
+			this.y + offset,
+			this.grid.w - this.grid.off,
+			this.grid.h - this.grid.off
+		  );
+		  context.stroke();
+		  context.fill();
+		}
+	
+		context.restore();
+	  }
+	}
+
+
+	const sketch = () => {
+		const squares = [];
+	  
+		return ({ context, width, height, time }) => {
+		  context.fillStyle = brown;
+		  context.fillRect(0, 0, width, height);
+
+		const w = width * 0.10;  
+		const h = height * 0.10;  
+		const gap = width * 0.03; 
+		const ix = width * 0.17; 
+		const iy = height * 0.17;
+
+		const off = width * 0.02; 
 		
-	let x, y;
+		context.lineWidth = width * 0.01;
+			
+		let x, y;
 
-	for (let i = 0; i < 5; i++){
-        for(let j = 0; j < 5; j++) {
-			x = ix + (w + gap) * i;
-			y = iy + (h + gap) * j;
+		for (let i = 0; i < 5; i++){
+			for(let j = 0; j < 5; j++) {
+				x = ix + (w + gap) * i;
+				y = iy + (h + gap) * j;
 
-			const squares = [];
-			squares.push(new Square(w, h, gap, ix, iy, off, x, y));	
+				squares.push(new Square(w, h, gap, ix, iy, off, x, y));	
 
-			squares.forEach(square => {
-				square.draw(context);
-			});
+				squares.forEach(square => {
+					square.draw(context, time * 0.2);
+				});
 
 		}
 	}
+
   };
+
 };
 
 canvasSketch(sketch, settings);
