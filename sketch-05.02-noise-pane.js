ed0bb0765cc61dfe3d2d070851ee6b9d711e9067
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-11-09 18:45:10 +0000

    formated code in noise pane

--- sketch-05.02-noise-pane.js
+++ sketch-05.02-noise-pane.js
@@ -4,7 +4,7 @@ const math = require('canvas-sketch-util/math')
 const Tweakpane = require('tweakpane')
 
 const settings = {
-  dimensions: [ 1080, 1080 ],
+  dimensions: [1080, 1080],
   animate: true
 };
 
@@ -22,8 +22,9 @@ const params = {
 
 const sketch = () => {
   return ({ context, width, height, frame }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
+    context.strokeStyle = 'white';
 
     const cols = params.cols;
     const rows = params.rows;
@@ -34,45 +35,45 @@ const sketch = () => {
     const cellw = gridw / cols;
     const cellh = gridh / rows;
     const margx = (width - gridw) * 0.5;
-    const margy = (height -gridh) * 0.5;
+    const margy = (height - gridh) * 0.5;
 
-for (let i = 0; i < numCells; i++) {
-  const col = i % cols;
-  const row = Math.floor(i / cols);
+    for (let i = 0; i < numCells; i++) {
+      const col = i % cols;
+      const row = Math.floor(i / cols);
 
-  const x = col * cellw;
-  const y = row * cellh;
-  const w = cellw * 0.8;
-  const h = cellh * 0.8;
+      const x = col * cellw;
+      const y = row * cellh;
+      const w = cellw * 0.8;
+      const h = cellh * 0.8;
 
-  const f = params.animate ? frame : params.frame;
+      const f = params.animate ? frame : params.frame;
 
-  //const n = random.noise2D(x + frame * 10, y, params.freq); with 2d is obvious the mov from right to left, we use instead 3d and frame is the 3rd param. The mov is more organic now.
-  const n = random.noise3D(x, y, f * 10, params.freq); 
-  
-  const angle = n * Math.PI * params.amp;  
+      //const n = random.noise2D(x + frame * 10, y, params.freq); with 2d is obvious the mov from right to left, we use instead 3d and frame is the 3rd param. The mov is more organic now.
+      const n = random.noise3D(x, y, f * 10, params.freq);
 
-  /* const n = random.noise2D(x + frame * 10,y, 0.001); 
-  const angle = n * Math.PI * 0.2;  */
+      const angle = n * Math.PI * params.amp;
 
-  const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
+      /* const n = random.noise2D(x + frame * 10,y, 0.001); 
+      const angle = n * Math.PI * 0.2;  */
 
-  context.save();
-  context.translate(x,y);
-  context.translate(margx, margy);
-  context.translate(cellw * 0.5, cellh * 0.5);
-  context.rotate(angle);
+      const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
 
-  context.lineWidth = scale;
-  context.lineCap = params.lineCap;
+      context.save();
+      context.translate(x, y);
+      context.translate(margx, margy);
+      context.translate(cellw * 0.5, cellh * 0.5);
+      context.rotate(angle);
 
-  context.beginPath();
-  context.moveTo(w * -0.5, 0);
-  context.lineTo(w * 0.5, 0);
-  context.stroke();
+      context.lineWidth = scale;
+      context.lineCap = params.lineCap;
 
-  context.restore();
-}
+      context.beginPath();
+      context.moveTo(w * -0.5, 0);
+      context.lineTo(w * 0.5, 0);
+      context.stroke();
+
+      context.restore();
+    }
 
   };
 };
@@ -81,16 +82,16 @@ const createPane = () => {
   const pane = new Tweakpane.Pane();
   let folder;
 
-  folder = pane.addFolder({title: 'Grid'});
-  folder.addInput(params, 'lineCap', { options: { butt: 'butt', round: 'round', square: 'square'}});
-  folder.addInput(params, 'cols', {min: 2, max: 50, step: 1});
-  folder.addInput(params, 'rows', {min: 2, max: 50, step: 1});
-  folder.addInput(params, 'scaleMin', {min: 1, max: 100});
-  folder.addInput(params, 'scaleMax', {min: 1, max: 100});
+  folder = pane.addFolder({ title: 'Grid' });
+  folder.addInput(params, 'lineCap', { options: { butt: 'butt', round: 'round', square: 'square' } });
+  folder.addInput(params, 'cols', { min: 2, max: 50, step: 1 });
+  folder.addInput(params, 'rows', { min: 2, max: 50, step: 1 });
+  folder.addInput(params, 'scaleMin', { min: 1, max: 100 });
+  folder.addInput(params, 'scaleMax', { min: 1, max: 100 });
 
-  folder = pane.addFolder({ title:'Noise'});
-  folder.addInput(params, 'freq', { min: -0.01, max: 0.01});
-  folder.addInput(params, 'amp', { min: 0, max: 1}); 
+  folder = pane.addFolder({ title: 'Noise' });
+  folder.addInput(params, 'freq', { min: -0.01, max: 0.01 });
+  folder.addInput(params, 'amp', { min: 0, max: 1 });
   folder.addInput(params, 'animate');
   folder.addInput(params, 'frame', { min: 0, max: 999 });
 }
@@ -98,6 +99,6 @@ const createPane = () => {
 createPane();
 canvasSketch(sketch, settings);
 
-//npx canvas-sketch sketch-noise-05.02-pane.js --open
+//npx canvas-sketch sketch-05.02-noise-pane.js --open
 
 //addding perhaps more params: speed of animation, lenght of each stroke, toggle to shift between different shapes
--- sketch-project-noise-1.js
+++ sketch-project-noise-1.js
@@ -7,7 +7,7 @@ const random = require('canvas-sketch-util/random');
 const math = require('canvas-sketch-util/math')
 
 const settings = {
-  dimensions: [ 1080, 1080 ],
+  dimensions: [1080, 1080],
 };
 
 // we need some vars, columns, rows and number of cells. Width and height for grid and cell, and pos x and y for margin.
@@ -28,67 +28,67 @@ const sketch = () => {
     const margx = (width - gridw) * 0.5;
     const margy = (height - gridh) * 0.5;
 
-// we are not going to use 2 for loops this time but maths
+    // we are not going to use 2 for loops this time but maths
 
-for (let i = 0; i < numCells; i++) {  
-  // cols = 4 and rows = 3
-  const col = i % cols; // while i increases in steps of 1, the value of col is modulating between 0 and 3, which is what we need to find the grid cells in the x axis
-  const row = Math.floor(i / cols);
-  // every 4 steps the value of rows is increasing by 1
+    for (let i = 0; i < numCells; i++) {
+      // cols = 4 and rows = 3
+      const col = i % cols; // while i increases in steps of 1, the value of col is modulating between 0 and 3, which is what we need to find the grid cells in the x axis
+      const row = Math.floor(i / cols);
+      // every 4 steps the value of rows is increasing by 1
 
-  const x = col * cellw;
-  const y = row * cellh;
-  const w = cellw * 0.8; // %80
-  const h = cellh * 0.8;
+      const x = col * cellw;
+      const y = row * cellh;
+      const w = cellw * 0.8; // %80
+      const h = cellh * 0.8;
 
-  const n = random.noise2D(x, y, 0.001);
-  // noise2d gives us a number between -1 and 1 and when multiplied by Math.PI we get the equivalent of - 180 degrees to 180 degrees and to see it we need to rotate our context by angle
-  //Right now the frequency is too high, because of the big dif between x and y, we fix adding the 3rd param in noise which is frequency. 
-  //We also add amplitud, not as 4th param of noise2D, but to const angle and the lines then start flowing nicely.
+      const n = random.noise2D(x, y, 0.001);
+      // noise2d gives us a number between -1 and 1 and when multiplied by Math.PI we get the equivalent of - 180 degrees to 180 degrees and to see it we need to rotate our context by angle
+      //Right now the frequency is too high, because of the big dif between x and y, we fix adding the 3rd param in noise which is frequency. 
+      //We also add amplitud, not as 4th param of noise2D, but to const angle and the lines then start flowing nicely.
 
-  const angle = n * Math.PI * 0.2; // 0.2 is amplitud  
+      const angle = n * Math.PI * 0.2; // 0.2 is amplitud  
 
 
-// These formulas are doing the same thing 
-//   const scale = (n + 1) / 2 * 30;
-//   const scale = (n * 0.5 + 0.5) * 30;
-  const scale = math.mapRange(n, -1, 1, 1, 20);
- //value, inputMin, inputMax, outputMin, outputMax, clamp = false)
+      // These formulas are doing the same thing 
+      //   const scale = (n + 1) / 2 * 30;
+      //   const scale = (n * 0.5 + 0.5) * 30;
+      const scale = math.mapRange(n, -1, 1, 1, 20);
+      //value, inputMin, inputMax, outputMin, outputMax, clamp = false)
 
-  context.save();
-  context.translate(x,y);
-  context.translate(margx, margy);
-  context.translate(cellw * 0.5, cellh * 0.5);
-  context.rotate(angle);
+      context.save();
+      context.translate(x, y);
+      context.translate(margx, margy);
+      context.translate(cellw * 0.5, cellh * 0.5);
+      context.rotate(angle);
 
-  context.lineWidth = scale;
-  context.strokeStyle = "white";
+      context.lineWidth = scale;
+      context.strokeStyle = "white";
 
-  // Drawing diamonds
-  // context.beginPath();
-  // context.moveTo(0, -h * 0.5); // Move to the top center of the diamond
-  // context.lineTo(w * 0.5, 0); // Draw a line to the right center
-  // context.lineTo(0, h * 0.5); // Draw a line to the bottom center
-  // context.lineTo(-w * 0.5, 0); // Draw a line to the left center
-  // context.closePath(); // Close the path to complete the diamond
-  // //context.fill();
-  // context.stroke();
+      // Drawing diamonds
+      // context.beginPath();
+      // context.moveTo(0, -h * 0.5); // Move to the top center of the diamond
+      // context.lineTo(w * 0.5, 0); // Draw a line to the right center
+      // context.lineTo(0, h * 0.5); // Draw a line to the bottom center
+      // context.lineTo(-w * 0.5, 0); // Draw a line to the left center
+      // context.closePath(); // Close the path to complete the diamond
+      // //context.fill();
+      // context.stroke();
 
-  //Drawing triangles
-  context.beginPath();
-  context.moveTo(0, -h * 0.5); // Move to the top center of the diamond
-  context.lineTo(w * 0.5, 0); // Draw a line to the right center
-  //context.lineTo(0, h * 0.5); // Draw a line to the bottom center
-  context.lineTo(-w * 0.5, 0); // Draw a line to the left center
-  context.closePath(); // Close the path to complete the diamond
-  //context.fill();
-  context.stroke();
-  context.restore();
-  
-   }
+      //Drawing triangles
+      context.beginPath();
+      context.moveTo(0, -h * 0.5); // Move to the top center of the diamond
+      context.lineTo(w * 0.5, 0); // Draw a line to the right center
+      //context.lineTo(0, h * 0.5); // Draw a line to the bottom center
+      context.lineTo(-w * 0.5, 0); // Draw a line to the left center
+      context.closePath(); // Close the path to complete the diamond
+      //context.fill();
+      context.stroke();
+      context.restore();
+
+    }
   };
 };
 
-canvasSketch(sketch, settings); 
+canvasSketch(sketch, settings);
 
-// npx canvas-sketch sketch-project-noise.js
+// npx canvas-sketch sketch-project-noise-1.js
